4dcd422e6f79288be3ea2e08710a9c1e4b0ca403
pisariev <86107013+pisariev@users.noreply.github.com>
2021-12-26 10:44:40 +0200

    add negative tests for auth spec
    add helper
    
    Co-authored-by: Sergiy <ipisaryev@gmail.com>

--- pages/login.page.js
+++ pages/login.page.js
@@ -4,6 +4,11 @@ class LoginPage extends Page {
     get inputUsername() { return $('[qa-id="email"]') }
     get inputPassword() { return $('[qa-id="password"]') }
     get buttonSubmit() { return $('.login-form-button') }
+    get notification() { return $('.ant-notification-notice-closable')}
+    get emailMessage() { return $('//div[contains(text(),"\'email\' is not a valid email")]')}
+    get passwordValidation() { return $('//form[@id=\'normal_login\']/div[2]/div[@class=\'ant-col ant-form-item-control\']')}
+    get emailValidation() { return $('//form[@id=\'normal_login\']/div[1]//div[@role=\'alert\']')}
+
 
     async login(username, password) {
         await this.inputUsername.setValue(username);
--- specs/auth.spec.js
+++ specs/auth.spec.js
@@ -14,4 +14,27 @@ describe('Auth', function() {
             .toBeDisplayed();
     });
 
+    it('the set password is invalid ', async function() {
+        await LoginPage.login('ipisaryev@gmail.com', 'invalid');
+        await expect(LoginPage.notification).toHaveTextContaining('Incorrect password')
+    });
+
+    it('User entered an unregistered email address or password ', async function () {
+        await LoginPage.login('example@gmail.com', 'example');
+        await expect(LoginPage.notification).toHaveTextContaining('Email is not registered');
+    });
+
+    it('value format check ', async function () {
+        await LoginPage.inputUsername.setValue('invalid');
+        await expect(LoginPage.emailMessage).toHaveTextContaining('\'email\' is not a valid email');
+    });
+
+    it('Credentials are required', async function() {
+        await LoginPage.inputUsername.setValue('invalid@example.com');
+        await LoginPage.inputUsername.smartClear();
+        await expect(LoginPage.emailValidation).toHaveTextContaining('Required');
+        await LoginPage.inputPassword.setValue('invalid');
+        await LoginPage.inputPassword.smartClear();
+        await expect(LoginPage.passwordValidation).toHaveTextContaining('Required');
+    });
 });
